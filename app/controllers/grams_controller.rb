af4616c3437ed0743baf3ae8543da2240bebaf8f
Alper-Enes-DURU <enesduru@gmail.com>
2019-10-13 23:44:47 +0000

    Found an error from last commit, still working on it.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,6 +1,15 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
+  def create
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
@@ -43,15 +52,6 @@ class GramsController < ApplicationController
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
     return render_not_found(:forbidden) if @gram.user != current_user
-  end
-
-  def create
-    @gram = current_user.grams.create(gram_params)
-    if @gram.valid?
-      redirect_to root_path
-    else
-      render :new, status: :unprocessable_entity
-    end
   end
 
   private
